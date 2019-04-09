d1ce0c109af4d2d881cfe175e05c7943d441c92c
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-04-09 11:30:12 -0400

    micro edit on results output

--- R/enrich_geneset.R
+++ R/enrich_geneset.R
@@ -81,7 +81,7 @@ enrich_geneset <- function(gene_set)
     dplyr::mutate(., enrichment_score = replace(enrichment_score, direction_call == "down", -1 * enrichment_score)) %>%
     dplyr::filter(., number_genes > 1) %>%
     dplyr::arrange(., desc(enrichment_score)) %>%
-    dplyr::select(., geneset, number_genes, cosine_similarity, probability_random, enrichment_score)
+    dplyr::select(., geneset, number_genes, cosine_similarity, probability_random, enrichment_score, direction_call)
 
 
   return(res)

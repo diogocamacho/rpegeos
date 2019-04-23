e981936516ca6256d21dfc6ba14afa8d98fec6e8
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-04-23 14:00:03 -0400

    edit in scoring method

--- R/enrich_geneset.R
+++ R/enrich_geneset.R
@@ -73,8 +73,7 @@ enrich_geneset <- function(gene_set)
 
   res <- res %>%
     dplyr::mutate(., probability_random = replace(probability_random, probability_random == 0, 1/ui[[3]])) %>%
-    dplyr::mutate(., enrichment_score = -log10(probability_random)) %>%
-    dplyr::mutate(., enrichment_score = enrichment_score + (cosine_similarity * number_genes) + 1) %>%
+    dplyr::mutate(., enrichment_score = cosine_similarity - log10(probability_random) + 1) %>%
     dplyr::filter(., number_genes > 1) %>%
     dplyr::arrange(., desc(enrichment_score))
 
